10b598659958819da2dd2046445c515fcabde61f
Alex Yang <alex.greekbase@gmail.com>
2023-01-18 22:05:01 -0800

    update setUpVim bash script

--- setUpVim.sh
+++ setUpVim.sh
@@ -1,6 +1,15 @@
 #!bin/sh
-echo; echo "Installing Vim package manager..."; sleep 1
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+if [ ! -d "$HOME/.vim/bundle/Vundle.vim" ]
+then
+    echo; echo "Installing Vim package manager..."; sleep 1
+    git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+else
+    echo "Pulling the latest Vundle updates"
+    cd ~/.vim/bundle/Vundle.vim
+    git pull
+    cd ~/.dotfiles
+fi
 
 echo; echo "Copying Vim config files..."; sleep 1
 cp config/.vimrc ~/.vimrc
+echo "done!"
