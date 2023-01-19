50b631991c3b7fc7a24b143d1c756fd5e8cb4d03
Alex Yang <alex.greekbase@gmail.com>
2023-01-18 22:03:42 -0800

    updated setUpZsh bash script

--- setUpZsh.sh
+++ setUpZsh.sh
@@ -3,22 +3,23 @@ echo; echo "Installing Zsh plugins..."; sleep 1
 if [ ! -d "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting" ]
 then    
   git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-  echo "Zsh syntax highlighting plugin installed!"
+  echo "Zsh syntax highlighting plugin installed!"; echo
 fi
 if [ ! -d "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-completions" ]
 then    
   git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:-${ZSH:-~/.oh-my-zsh}/custom}/plugins/zsh-completions
-  echo "Zsh completions plugin installed!"
+  echo "Zsh completions plugin installed!"; echo
 fi
 if [ ! -d "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions" ]
 then    
   git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-  echo "Zsh autosuggestions plugin installed!"
+  echo "Zsh autosuggestions plugin installed!"; echo
 fi
 
 echo; echo "Copying zsh config files..."; sleep 1;
 cp config/.zshrc ~/.zshrc
-echo ".zshrc file updated"
+echo ".zshrc file updated"; echo
+
 if [ ! -f "$HOME/.zshlocalrc" ]
 then
   cat ~/.zshlocalrc ~/.zshlocalrc_backup
