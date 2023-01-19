98b9b5e002b44acc0ed02a4705bf85eec9a96d79
Alex Yang <alex.greekbase@gmail.com>
2023-01-18 21:16:10 -0800

    correct log text

--- setUpVim.sh
+++ setUpVim.sh
@@ -2,5 +2,5 @@
 echo; echo "Installing Vim package manager..."; sleep 1
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
-echo; echo "Copying tmux config files..."; sleep 1
+echo; echo "Copying Vim config files..."; sleep 1
 cp config/.vimrc ~/.vimrc
