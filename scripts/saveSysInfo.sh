adaaba70b129d3835392277aaa0d12bcbd8de226
Alex Yang <alex.hyang.dev@gmail.com>
2023-01-20 02:31:56 -0800

    correct path to sysinfo

--- scripts/saveSysInfo.sh
+++ scripts/saveSysInfo.sh
@@ -2,9 +2,9 @@
 
 if [ ! -f "~/sysinfo" ]
 then
-    echo "Installed Date:" >> sysinfo
-    date >> sysinfo
-    echo "\n" >> sysinfo
-    echo "OS Info:" >> sysinfo
-    grep -E '^(VERSION|NAME)=' /etc/os-release >> sysinfo
+    echo "Installed Date:" >> ~/sysinfo
+    date >> ~/sysinfo
+    echo "" >> ~/sysinfo
+    echo "OS Info:" >> ~/sysinfo
+    grep -E '^(VERSION|NAME)=' /etc/os-release >> ~/sysinfo
 fi
