0b4fd8c593f346467eb59b87422cb6f6a9cf26c4
Alex Yang <alex.hyang.dev@gmail.com>
2023-01-26 17:36:08 -0800

    update paths

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd ~/.dotfiles
+DOTFILES="$HOME/.dotfiles"
 
 sudo apt install tree htop  # install more terminal packages
 apt list --installed curl git zsh vim tmux tree htop 
@@ -8,12 +8,12 @@ apt list --installed curl git zsh vim tmux tree htop
 # curl -V, git version, zsh --version, vim --version
 # tmux -V, nvm --version, node --version
 
-echo; echo "Settings up zsh..."; source scripts/setUpZsh.sh
-echo; echo "Settings up vim..."; source scripts/setUpVim.sh
-echo; echo "Settings up tmux..."; source scripts/setUpTmux.sh
-echo; echo "Copying config files..."; source updateConfig.sh
-echo; echo "Now install Vim plugins in editor..."; source scripts/setUpVimTheme.sh
-echo; echo "Saving system information..."; source scripts/saveSysInfo.sh
+echo; echo "Settings up zsh..."; source $DOTFILES/scripts/setUpZsh.sh
+echo; echo "Settings up vim..."; source $DOTFILES/scripts/setUpVim.sh
+echo; echo "Settings up tmux..."; source $DOTFILES/scripts/setUpTmux.sh
+echo; echo "Copying config files..."; source $DOTFILES/updateConfig.sh
+echo; echo "Now install Vim plugins in editor..."; source $DOTFILES/scripts/setUpVimTheme.sh
+echo; echo "Saving system information..."; source $DOTFILES/scripts/saveSysInfo.sh
 
 cd ~
 echo "Automatic setups finished!"
