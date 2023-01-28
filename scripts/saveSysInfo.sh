266682300f6520a43e842669dd65732002409db8
Alex Yang <alex.hyang.dev@gmail.com>
2023-01-28 14:22:54 -0800

    improve sysinfo script

--- scripts/saveSysInfo.sh
+++ scripts/saveSysInfo.sh
@@ -2,9 +2,7 @@
 
 if [ ! -f "$HOME/sysinfo" ]
 then
-    echo "Installed Date:" >> ~/sysinfo
-    date >> ~/sysinfo
-    echo "" >> ~/sysinfo
     echo "OS Info:" >> ~/sysinfo
+    date "+DATE='%Y-%m-%d %T %Z'" >> ~/sysinfo
     grep -E '^(VERSION|NAME)=' /etc/os-release >> ~/sysinfo
 fi
