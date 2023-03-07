329dca37b099406b97a47899bd4f2c7bc22c1ead
Alex Yang <alex.hyang.dev@gmail.com>
2023-03-07 13:45:00 -0800

    refactor shell scripts

--- scripts/saveSysInfo.sh
+++ scripts/saveSysInfo.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
+echo "Saving system information..."
 
 if [ ! -f "$HOME/sysinfo" ]
 then
     echo "OS Info:" >> ~/sysinfo
     date "+DATE='%Y-%m-%d %T %Z'" >> ~/sysinfo
     grep -E '^(VERSION|NAME)=' /etc/os-release >> ~/sysinfo
+    echo "MACHINE=<update information here>" >> ~/sysinfo
+    vim ~/sysinfo
     echo "~/sysinfo created!"
 fi
 
