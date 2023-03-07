88194b966fc41f738b30de04093c3064fa5a14f0
Alex Yang <alex.greekbase@gmail.com>
2023-03-06 16:01:39 -0800

    add log message

--- scripts/saveSysInfo.sh
+++ scripts/saveSysInfo.sh
@@ -5,4 +5,6 @@ then
     echo "OS Info:" >> ~/sysinfo
     date "+DATE='%Y-%m-%d %T %Z'" >> ~/sysinfo
     grep -E '^(VERSION|NAME)=' /etc/os-release >> ~/sysinfo
+    echo "~/sysinfo created!"
 fi
+
