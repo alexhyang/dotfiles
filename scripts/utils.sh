f7509abb3b4396d4b9f9663d2d22d09c96713ff3
Alex Yang <alex.hyang.dev@gmail.com>
2023-03-07 14:08:49 -0800

    refactor shell scripts

--- scripts/utils.sh
+++ scripts/utils.sh
@@ -8,14 +8,20 @@ gitpull () {
   fi
 }
 
-create_dotconfig_file () {
+create_dotfile () {
   # create local config files
   echo; echo "Creating $2 config file...";
   cp ~/.dotfiles/config/$1 ~/$1
   echo "~/$1 created!"
 }
 
-update_dotconfig_file () {
+create_unique_local_dotfile () {
+  if [[ ! -f ~/$1 ]]; then
+    create_dotfile $1 $2
+  fi
+}
+
+update_dotfile () {
   # overwrite local config with remote config
   if ! diff ~/$1 ~/.dotfiles/config/$1; then
     echo "~/.dotfiles/config/$1 --> ~/$1? [y/n]"
@@ -29,7 +35,7 @@ update_dotconfig_file () {
   fi
 }
 
-backupdate_dotconfig_file () {
+backupdate_dotfile () {
   # overwrite remote config with local config
   if ! diff ~/.dotfiles/config/$1 ~/$1 ; then
     echo "~/$1 --> ~/.dotfiles/config/$1? [y/n]"
@@ -43,10 +49,10 @@ backupdate_dotconfig_file () {
   fi
 }
 
-create_update_dotconfig () {
+create_update_dotfile () {
   if [ ! -e ~/$1 ]; then
-    create_dotconfig_file $1 $2
+    create_dotfile $1 $2
   else
-    update_dotconfig_file $1
+    update_dotfile $1
   fi
 }
