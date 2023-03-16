cf4bf7337ef888e6c69f1832af29488e8f0e87e8
Alex Yang <alex.hyang.dev@gmail.com>
2023-03-16 14:01:53 -0700

    rename .zshrc.aliases -> .aliases

--- scripts/createSymLinks.sh
+++ scripts/createSymLinks.sh
@@ -3,7 +3,7 @@ echo "Linking dotfiles...;"
 
 ln -s $DOTFILES/config/.zshrc ~/.zshrc
 ln -s $DOTFILES/config/.p10k.zsh ~/.p10k.zsh
-ln -s $DOTFILES/config/.zshrc.alias ~/.zshrc.alias
+ln -s $DOTFILES/config/.aliases ~/.aliases
 ln -s $DOTFILES/config/.vimrc ~/.vimrc
 ln -s $DOTFILES/config/.tmux.conf ~/.tmux.conf
 mkdir -p ~/.tmux
