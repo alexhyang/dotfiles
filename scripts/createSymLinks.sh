b58ee86c512c6c2e2a2bb603c68363cca8e169a4
Alex Yang <alex.hyang.dev@gmail.com>
2023-03-26 22:09:18 -0700

    separate shell functions from .aliases

--- scripts/createSymLinks.sh
+++ scripts/createSymLinks.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 echo "Linking dotfiles...;"
+DOTFILES="$HOME/.dotfiles"
 
 # shell
 ln -s $DOTFILES/config/.aliases ~/.aliases
+ln -s $DOTFILES/config/.shell_utils ~/.shell_utils
 ln -sf $DOTFILES/config/.profile ~/.profile
 
 ln -sf $DOTFILES/config/.bashrc ~/.bashrc
-ln -sf $DOTFILES/config/.bash_aliases ~/.bash_aliases
+ln -s $DOTFILES/config/.bash_aliases ~/.bash_aliases
 
 ln -sf $DOTFILES/config/.zshrc ~/.zshrc
 ln -s $DOTFILES/config/.p10k.zsh ~/.p10k.zsh
