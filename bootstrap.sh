75208436801254b502c2c5a2a8f7d22d5c1d7d82
Alex Yang <alex.hyang.dev@gmail.com>
2023-08-04 13:22:00 -0700

    rename scripts

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,14 +7,15 @@ create_unique_local_dotfile .gitconfig git
 create_unique_local_dotfile .local_env local_env
 
 # universal setup
-source $DOTFILES/scripts/setUpCLTs.sh
-source $DOTFILES/scripts/setUpZsh.sh
-source $DOTFILES/scripts/createSymLinks.sh
-source $DOTFILES/scripts/setUpVim.sh
-source $DOTFILES/scripts/setUpTmux.sh
-source $DOTFILES/scripts/saveSysInfo.sh
+source $DOTFILES/scripts/set_up_command_line_tools.sh
+source $DOTFILES/scripts/set_up_zsh.sh
+source $DOTFILES/scripts/create_symlinks.sh
+source $DOTFILES/scripts/set_up_vim.sh
+source $DOTFILES/scripts/set_up_tmux.sh
+source $DOTFILES/scripts/save_sysinfo.sh
 
 echo "Automatic setups finished!"
 echo "New Machine Configuration Finished!\n"
 cat ~/sysinfo
-echo "Reloading zsh..."; omz reload
+echo "Reloading zsh..."
+omz reload
