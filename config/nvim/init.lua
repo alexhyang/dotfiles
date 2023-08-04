e50a4d3bedf56ef3b575770b13d3d4861f43951a
Alex Yang <alex.hyang.dev@gmail.com>
2023-08-04 00:28:13 -0700

    customize nvim

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,18 +1,2 @@
 require("config.lazy")
--- Set some basic settings
--- vim.cmd("syntax enable")
--- vim.cmd("filetype plugin indent on")
--- vim.cmd("set number")
--- vim.cmd("set relativenumber")
--- vim.cmd("set expandtab")
--- vim.cmd("set tabstop=2")
--- vim.cmd("set shiftwidth=2")
--- vim.cmd("set softtabstop=2")
--- vim.cmd("set splitright")
--- vim.cmd("set mouse=a")
--- vim.cmd("set backspace=indent,eol,start")
--- vim.cmd("set noerrorbells visualbell t_vb=")
-
--- vim.g.catppuccin_flavour = "macchiato" -- latte, frappe, macchiato, mocha
--- require("catppuccin").setup()
--- vim.cmd('colorscheme catppuccin')
+require("lazy").plugins()
