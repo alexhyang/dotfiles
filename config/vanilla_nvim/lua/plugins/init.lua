87205280f3c275464833205f01cdfec149179853
Alex Yang <alex.hyang.dev@gmail.com>
2023-08-08 22:21:32 -0700

    set up vanilla nvim config

---
+++ config/vanilla_nvim/lua/plugins/init.lua
@@ -0,0 +1,31 @@
+require("lazy").setup({
+  "folke/which-key.nvim",
+  { "folke/tokyonight.nvim", lazy = true, opts = { style = "moon" } },
+  { "jose-elias-alvarez/null-ls.nvim", dependencies = { "nvim-lua/plenary.nvim" } },
+  { "nvim-lualine/lualine.nvim", dependencies = { "nvim-tree/nvim-web-devicons" } },
+  { "neovim/nvim-lspconfig" },
+  { "williamboman/mason.nvim" },
+  { "echasnovski/mini.nvim" },
+})
+
+-- null-ls
+require("null-ls")
+local null_ls = require("null-ls")
+null_ls.setup({
+  sources = {
+    null_ls.builtins.formatting.stylua,
+    null_ls.builtins.diagnostics.eslint,
+    null_ls.builtins.completion.spell,
+  },
+})
+
+require("lualine").setup() -- lualine
+require("plugins.lsp-config") -- lsp-config
+require("mason").setup() -- Mason
+
+require("mini.pairs").setup()
+require("mini.surround").setup()
+require("mini.comment").setup()
+require("mini.indentscope").setup()
+require("mini.completion").setup()
+
