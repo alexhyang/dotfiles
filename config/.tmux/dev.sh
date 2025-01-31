116b1099b4d6ba6fc30127d6594046aa9e925e0e
Alex Yang <alex.hyang.dev@gmail.com>
2025-01-30 16:14:05 -0800

    update tmux config

--- config/.tmux/dev.sh
+++ config/.tmux/dev.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 source-file ~/.tmux.conf
 
-# window 0: ref
-new-session -s dev -n ref
+# window 0: shell
+new-session -s dev -n shell
 
 # window 1: ide
 new-window -n ide
 split-window -v -p 20
 select-pane -t 0
 
-# window 2: shell
-new-window -n shell
+# window 2: ref
+new-window -n ref
 select-pane -t 0
 
 select-window -t dev:1
