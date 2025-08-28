8d8af6f0bea6246f7c78cd8aefc649e6a2ad63e4
Alex Yang <alex.hyang.dev@gmail.com>
2025-08-27 20:47:21 -0700

    rename file

---
+++ scripts/link_dotfiles.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+echo "Linking dotfiles...;"
+CONFIG="$HOME/.dotfiles/config"
+
+# shell
+# -s creates a soft link
+# -f replaces existing link if there's any
+ln -s $CONFIG/.aliases ~/.aliases
+ln -s $CONFIG/.shell_utils ~/.shell_utils
+
+ln -sf $CONFIG/.bashrc ~/.bashrc
+ln -s $CONFIG/.bash_aliases ~/.bash_aliases
+
+# workflow
+ln -sf $CONFIG/.vimrc ~/.vimrc
+ln -sf $CONFIG/.tmux.conf ~/.tmux.conf
+ln -sfn $CONFIG/.tmux ~/.tmux
+
+echo "dotflies linked successfully!"
