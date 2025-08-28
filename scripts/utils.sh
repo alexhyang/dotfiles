6356d0a45cbb597b210349f937fb44f13ab5ea8c
Alex Yang <alex.hyang.dev@gmail.com>
2025-08-27 19:09:53 -0700

    clean utils scripts

--- scripts/utils.sh
+++ scripts/utils.sh
@@ -1,3 +1,6 @@
+#!/bin/bash
+# This file provides utilities for bootstrap.sh
+
 gitpull() {
   if [[ $(pwd) == $1 ]]; then
     git pull
@@ -8,64 +11,13 @@ gitpull() {
   fi
 }
 
-create_dotfile() {
-  # create local config files
-  echo
-  echo "Creating $2 config file..."
-  cp ~/.dotfiles/config/$1 ~/$1
-  echo "~/$1 created!"
+copy_dotfile() {
+  echo "copying $1 to \$HOME/ ..."
+  cp -iv ~/.dotfiles/config/$1 ~/$1
 }
 
-create_unique_local_dotfile() {
-  if [[ ! -f ~/$1 ]]; then
-    create_dotfile $1 $2
-  fi
-}
-
-: <<'END'
-update_dotfile () {
-  # overwrite local config with remote config
-  if ! diff ~/$1 ~/.dotfiles/config/$1; then
-    promptMessage="~/.dotfiles/config/$1 --> ~/$1? [y/N]: "
-    if [[ $SHELL == "/usr/bin/zsh" ]]; then
-      read "confirm?$promptMessage"
-    else
-      read -p $promptMessage confirm
-    fi
-    if [[ $confirm =~ ^[Yy]$ ]]; then
-      cp ~/.dotfiles/config/$1 ~/$1
-      echo "~/$1 updated!\n"
-    else
-      echo "overwrite ~/$1 cancelled"
-    fi
-  fi
-}
-
-backupdate_dotfile () {
+backcopy_dotfile () {
   # overwrite remote config with local config
-  if ! diff ~/.dotfiles/config/$1 ~/$1 ; then
-
-    promptMessage="~/$1 --> ~/.dotfiles/config/$1? [y/N]: "
-    if [[ $SHELL == "/usr/bin/zsh" ]]; then
-      read "confirm?$promptMessage"
-    else
-      read -p $promptMessage confirm
-    fi
-    if [[ $confirm =~ ^[Yy]$ ]]; then
-      cp ~/$1 ~/.dotfiles/config/$1
-      echo "~/.dotfiles/config/$1 updated!\n"
-    else
-      echo "overwrite ~/.dotfiles/config/$1 cancelled"
-    fi
-  fi
+  echo "~/.dotfiles/config/$1 updated!\n"
+  cp ~/$1 ~/.dotfiles/config/$1
 }
-
-create_update_dotfile () {
-  # update dotfile, if not exists, create one
-  if [ ! -e ~/$1 ]; then
-    create_dotfile $1 $2
-  else
-    update_dotfile $1
-  fi
-}
-END
