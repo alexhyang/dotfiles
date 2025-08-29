d3355562c9b261abd8c67c78ad87b2aec8c7b761
Alex Yang <alex.hyang.dev@gmail.com>
2025-08-29 04:36:39 -0700

    add tmux setup workflow

--- scripts/set_up_tmux.sh
+++ scripts/set_up_tmux.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 echo "Setting up tmux...";
+
+echo "Installing catppuccin theme"
+mkdir -p ~/.config/tmux/plugins/catppuccin
+git clone -b v2.1.3 https://github.com/catppuccin/tmux.git ~/.config/tmux/plugins/catppuccin/tmux
+
 echo "tmux configuration finished!"
