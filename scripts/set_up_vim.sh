a11bdbe61dd4bd3ad17dcaf6a497c5c32a6f8a87
Alex Yang <alex.hyang.dev@gmail.com>
2025-08-29 04:03:50 -0700

    improve vim setup workflow

--- scripts/set_up_vim.sh
+++ scripts/set_up_vim.sh
@@ -1,17 +1,6 @@
 #!/bin/bash
 echo "Setting up Vim...";
 
-# set up vim package manger: Vundle (replaced by vim-plug seen below)
-# if [ ! -d "$HOME/.vim/bundle/Vundle.vim" ]
-# then
-    # echo "Installing Vim package manager...";
-    # git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-# else
-    # echo "Pulling the latest Vundle updates";
-    # gitpull ~/.vim/bundle/Vundle.vim
-# fi
-
-
 # Set up vim package manager: vim-plug
 echo "Copying plug.vim..."
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
@@ -29,34 +18,26 @@ fi
 
 # Install plugins in .vimrc
 echo "Install plugins in Vim..."
-if [ ! -d "$HOME/.vim/plugged/ale" ]
-then
-    echo "Last step: \nrun :PlugInstall" >>  temp.md
-    vim temp.md
-    rm temp.md
-    echo "Vim plugins installed!";
-fi
+vim +PlugInstall +qall
 
+# Set up themes
+echo "Copying theme color file..."
+mkdir -p $HOME/.vim/colors/
+cp -iv $HOME/.vim/plugged/vim-colors-solarized/colors/* $HOME/.vim/colors
+cp -iv $HOME/.vim/plugged/catppuccin/colors/* $HOME/.vim/colors
+
+# Set up youcompleteme
 promptMessage="Install youcompleteme plugin? [y/N]: "
 if [[ $SHELL == "/usr/bin/zsh" ]]; then
   read "confirm?$promptMessage"
-else
-  read -p $promptMessage confirm
 fi
+if [[ $SHELL == "/bin/bash" || $SHELL == "/usr/bin/bash" ]]; then
+  read -p "$promptMessage" confirm
+fi
+
 if [[ $confirm =~ ^[Yy]$ && -d "$HOME/.vim/plugged/youcompleteme" ]]; then
   sudo apt install cmake
   python3 ~/.vim/plugged/youcompleteme/install.py --ts-completer
 fi
-
-
-# Set up themes
-if [[ -d "$HOME/.vim/plugged/vim-colors-solarized"  &&  ! -f "$HOME/.vim/colors/solarized.vim" ]]
-then
-    echo "Copying theme color file..."
-    mkdir -p $HOME/.vim/colors/
-    cp $HOME/.vim/plugged/vim-colors-solarized/colors/solarized.vim $HOME/.vim/colors/solarized.vim
-    echo "solarized.vim theme configured!"
-fi
-
 
 echo "Vim configuration finished!"
