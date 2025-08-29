b433c96108f104d5889759547d635b0e854d331c
Alex Yang <alex.hyang.dev@gmail.com>
2025-08-29 04:41:28 -0700

    update bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,19 +3,16 @@ export DOTFILES="$HOME/.dotfiles"
 source $DOTFILES/scripts/utils.sh
 
 # local setup
-create_unique_local_dotfile .gitconfig git
-create_unique_local_dotfile .zshrc.local .zshrc.local
+copy_dotfile .gitconfig
+copy_dotfile .localrc
 
 # universal setup
+source $DOTFILES/scripts/link_dotfiles.sh
 source $DOTFILES/scripts/set_up_command_line_tools.sh
-source $DOTFILES/scripts/set_up_zsh.sh
-source $DOTFILES/scripts/create_symlinks.sh
-source $DOTFILES/scripts/set_up_vim.sh
 source $DOTFILES/scripts/set_up_tmux.sh
+source $DOTFILES/scripts/set_up_vim.sh
 source $DOTFILES/scripts/save_sysinfo.sh
 
 echo "Automatic setups finished!"
 echo "New Machine Configuration Finished!\n"
 cat ~/sysinfo
-echo "Reloading zsh..."
-omz reload
