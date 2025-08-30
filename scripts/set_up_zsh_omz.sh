959c1d77409f6be0e8fa91860f86420c428ac847
Alex Yang <alex.hyang.dev@gmail.com>
2025-08-29 18:56:15 -0700

    rename p10k for omz

--- scripts/set_up_zsh_omz.sh
+++ scripts/set_up_zsh_omz.sh
@@ -3,28 +3,28 @@
 echo "OhMyZsh setup starting..."
 
 ln -sf $DOTFILES/config/.zshrc.omz ~/.zshrc
-ln -s $DOTFILES/config/.p10k.zsh ~/.p10k.zsh
+ln -sf $DOTFILES/config/.p10k.zsh.omz ~/.p10k.zsh
 
 if [ ! -d "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting" ]
-then    
+then
   git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
   echo "Zsh syntax highlighting plugin installed!"
 fi
 
 if [ ! -d "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-completions" ]
-then    
+then
   git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:-${ZSH:-~/.oh-my-zsh}/custom}/plugins/zsh-completions
   echo "Zsh completions plugin installed!"
 fi
 
 if [ ! -d "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions" ]
-then    
+then
   git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
   echo "Zsh autosuggestions plugin installed!"
 fi
 
 if [ ! -d "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/powerlevel10k" ]
-then    
+then
   git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
   echo "Zsh powerlevel10k installed!"
 fi
