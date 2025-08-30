f3aa17c5434e12a8901573a73730a62c7f6eb085
Alex Yang <alex.hyang.dev@gmail.com>
2025-08-29 19:33:53 -0700

    use neofetch for sysinfo display

--- bootstrap.sh
+++ bootstrap.sh
@@ -11,8 +11,4 @@ source $DOTFILES/scripts/link_dotfiles.sh
 source $DOTFILES/scripts/set_up_command_line_tools.sh
 source $DOTFILES/scripts/set_up_tmux.sh
 source $DOTFILES/scripts/set_up_vim.sh
-source $DOTFILES/scripts/save_sysinfo.sh
-
-echo "Automatic setups finished!"
-echo "New Machine Configuration Finished!\n"
-cat ~/sysinfo
+neofetch
