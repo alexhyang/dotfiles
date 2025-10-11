ac1ab8f0934deaf3dd0467f46ee95aa0be2e41c7
Alex Yang <alex.hyang.dev@gmail.com>
2025-10-11 02:04:21 -0700

    update configs

--- scripts/link_dotfiles.sh
+++ scripts/link_dotfiles.sh
@@ -7,6 +7,7 @@ CONFIG="$HOME/.dotfiles/config"
 # -f replaces existing link if there's any
 ln -s $CONFIG/.aliases ~/.aliases
 ln -s $CONFIG/.shell_utils ~/.shell_utils
+ln -s $CONFIG/.p10k.zsh.omz ~/.p10k.zsh.omz
 
 ln -sf $CONFIG/.bashrc ~/.bashrc
 ln -s $CONFIG/.bash_aliases ~/.bash_aliases
