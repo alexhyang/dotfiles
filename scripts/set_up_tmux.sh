e87d4cfba505eaf3e79ef58c8451901b6ab61213
Alex Yang <alex.hyang.dev@gmail.com>
2026-07-23 06:43:47 -0700

    show live pomo timer in tmux bar

--- scripts/set_up_tmux.sh
+++ scripts/set_up_tmux.sh
@@ -5,4 +5,11 @@ echo "Installing catppuccin theme"
 mkdir -p ~/.config/tmux/plugins/catppuccin
 git clone -b v2.3.0 https://github.com/catppuccin/tmux.git ~/.config/tmux/plugins/catppuccin/tmux
 
+# integrate show live pomo timer in status bar
+# install pomo with: `go install github.com/Bahaaio/pomo@latest`
+
+POMO_CONFIG_ROOT="$HOME/.config/pomo"
+mkdir -p $POMO_CONFIG_ROOT
+ln -snf $HOME/.dotfiles/config/.tmux/pomo.yaml $POMO_CONFIG_ROOT/pomo.yaml
+
 echo "tmux configuration finished!"
