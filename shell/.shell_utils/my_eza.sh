97168867790198e45ff8b1a1e0f45a5f18e1bde4
Alex Yang <alex.hyang.dev@gmail.com>
2026-08-13 03:02:06 -0700

    add aliases for eza

--- shell/.shell_utils/my_eza.sh
+++ shell/.shell_utils/my_eza.sh
@@ -1,1 +1,17 @@
 #!/usr/bin/env bash
+
+alias l="eza --icons"
+alias lsa="eza -lah"
+alias la="eza -lAh"
+alias l.="eza -d .*"
+alias ll="eza -lh"
+alias ll.="eza -lh -d .*"
+lt() {
+  eza --tree -L ${1:-1}
+}
+lta() {
+  eza -a --tree -L ${1:-1}
+}
+lt.() {
+  eza -d .* --tree -L ${1:-1}
+}
