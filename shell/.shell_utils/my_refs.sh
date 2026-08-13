696823b054ffa930e3226cc2d63153a691a40b68
Alex Yang <alex.hyang.dev@gmail.com>
2026-08-12 22:27:22 -0700

    refactor

--- shell/.shell_utils/my_refs.sh
+++ shell/.shell_utils/my_refs.sh
@@ -9,72 +9,76 @@ q_refs() { # show help of specified command
   fi
 
   if [[ ! -f "/tmp/$1.ch_results" ]]; then
-    curl --silent "cheat.sh/$1" > "/tmp/$1.ch_results"
+    curl --silent "cheat.sh/$1" >"/tmp/$1.ch_results"
   fi
 
   local pattern="${2:-}"
   rg -B 1 -N "$2" "/tmp/$1.ch_results"
 }
 
+refs() { # get help from my local references
+  local refroot="$HOME/vimwiki/notes"
+  local refname target
+  local useglow=false
+  local OPTIND=1 opt
 
-# List/open/create my personal terminal command helps
-# if there is no argument, list available refs
-# if ref name is given, open or create markdown file
-# TODO: improve this function
-my_refs() { # get help from my references
-  createRef() {
-    promptMsg="Create $1? [y/N]"
-    if util_confirm "Create $1?"; then
-      touch $1 && vi $1
+  list_refs() {
+    fd -e md | sort | less
+  }
+
+  show_refs() {
+    refname="$1"
+    target="$1.md"
+
+    if [[ ! -f "$target" ]]; then
+      fd "$refname" -t f
+      return 1
+    fi
+
+    if $useglow; then
+      glow -p $target
+    else
+      bat $target
     fi
   }
 
-  refroot=~/vimwiki/notes
-  hop_in $refroot
-
-  if [[ $# -eq 0 && -d $refroot ]]; then
-    # $ myrefs
-    fd -e md | sort | less
-  else
-    if [[ $# -eq 1 ]]; then
-      refname="$1"
-    fi
-    if [[ $# -eq 2 ]]; then
-      flag="$1"
-      refname="$2"
-    fi
-
-    refpath="$refname.md"
-    if [[ -f $refpath ]]; then
-      # if refname.md exists
-      case $flag in
-        -e)
-          # $ myrefs -e xyz
-          vi $refpath
-          ;;
-        -g)
-          glow $refpath
-          ;;
-        *)
-          # $ myrefs <any other flags> xyz
-          bat $refpath
-          ;;
-      esac
-    else
-      # if refname.md does not exist
-      case $flag in
-        # -s)
-        #   # $ myrefs -s xyz
-        #   # search pattern in wiki/notes
-        #   fd $refname -t f
-        #   ;;
-        *)
-          fd $refname -t f
-          echo "Requested ref doesn't exit!"
-          createRef $refpath
-          ;;
-      esac
-    fi
+  # check
+  if [[ ! -d "$refroot" ]]; then
+    echo "$refroot doesn't exit. Aborting..."
+    return 1
   fi
-  hop_out > /dev/null
+
+  while getopts ":e:gh" opt; do
+    case "$opt" in
+      e)
+        vi "$refroot/$OPTARG.md"
+        return 0
+        ;;
+      g)
+        useglow=true
+        ;;
+      h)
+        echo "Usage: refs [-e] [-g] [target]"
+        return 0
+        ;;
+      \?)
+        echo "Unknown flag: -$OPTARG" >&2
+        return 1
+        ;;
+      :)
+        echo "Option -$OPTARG requires an argument" >&2
+        return 1
+        ;;
+    esac
+  done
+  shift $((OPTIND - 1))
+
+  hop_in $refroot
+  if [[ $# -eq 0 ]]; then
+    list_refs
+  else
+    show_refs $1
+  fi
+  hop_out >/dev/null
+  return 0
 }
