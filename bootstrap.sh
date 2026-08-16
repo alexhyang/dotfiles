6eb52a3e14ba460800bb819df12a58564921c376
Alex Yang <alex.hyang.dev@gmail.com>
2026-08-16 06:12:06 -0700

    fix file paths

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,8 +3,8 @@ export DOTFILES="$HOME/.dotfiles"
 source $DOTFILES/bootstrap/utils.sh
 
 # universal setup
-source $DOTFILES/scripts/set_up_command_line_tools.sh
+source $DOTFILES/bootstrap/set_up_command_line_tools.sh
 stow shell bash zsh-omz vim tmux proj-common
-source $DOTFILES/scripts/set_up_tmux.sh
-source $DOTFILES/scripts/set_up_vim.sh
+source $DOTFILES/bootstrap/set_up_tmux.sh
+source $DOTFILES/bootstrap/set_up_vim.sh
 fastfetch
