8e0cfcdf23ef3ad4b4642638b6bb46abe9273f9e
Alex Yang <alex.hyang.dev@gmail.com>
2026-08-15 20:10:32 -0700

    improve eza aliases

--- shell/.shell_utils/my_eza.sh
+++ shell/.shell_utils/my_eza.sh
@@ -7,11 +7,11 @@ alias l.="eza -d .*"
 alias ll="eza -lh"
 alias ll.="eza -lh -d .*"
 lt() {
-  eza --tree -L ${1:-1}
+  eza --tree -L ${1:-1} ${2:-.}
 }
 lta() {
-  eza -a --tree -L ${1:-1}
+  eza -a --tree -L ${1:-1} ${2:-.}
 }
 lt.() {
-  eza -d .* --tree -L ${1:-1}
+  eza -d .* --tree -L ${1:-1} ${2:-.}
 }
