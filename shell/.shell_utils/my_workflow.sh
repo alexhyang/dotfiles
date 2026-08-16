e2a858a7f8a51407c845f607e4467b1648fdc93b
Alex Yang <alex.hyang.dev@gmail.com>
2026-08-15 20:14:26 -0700

    update my_workflow aliases

--- shell/.shell_utils/my_workflow.sh
+++ shell/.shell_utils/my_workflow.sh
@@ -33,15 +33,16 @@ check_env_vars OBS_ROOT
 WEEKS_ROOT="$OBS_ROOT/Alex-weeks"
 TODO_ROOT="$WEEKS_ROOT/.todo"
 
-alias wiki="cd $VIM_WIKI_ROOT"
+alias .v="cd $VIM_WIKI_ROOT"
 alias ww="hop_in $VIM_WIKI_ROOT; vm index.md; hop_out > /dev/null"
-alias weeks="cd $WEEKS_ROOT"
+alias .w="cd $WEEKS_ROOT"
 wk() { # go to WEEKS_ROOT/ and open the lastest week note in (n)vim
   weeks
   vi $(realpath $(fd 2026- $WEEKS_ROOT | tail -n 1))
 }
 
 # Todo's
+alias .t="cd $TODO_ROOT"
 alias todo="todo.sh"
 alias tdcfg="nv ~/.config/todo/config"
 alias tde="vi $TODO_ROOT/todo.txt"
